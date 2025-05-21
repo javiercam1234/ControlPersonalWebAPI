54aa9d6fab2d0a83b35f9c4cd19da3e511ae7de9
javiercam1234 <javiercam1234@hotmail.com>
2025-05-21 02:06:14 -0600

    se agregan las tablas al proyecto

--- ControlPersonalWebAPI.Entidades/obj/Debug/net8.0/ControlPersonalWebAPI.Entidades.AssemblyInfo.cs
+++ ControlPersonalWebAPI.Entidades/obj/Debug/net8.0/ControlPersonalWebAPI.Entidades.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControlPersonalWebAPI.Entidades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1899ae27fb2532f89d2276ff02b7009cd591e3e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControlPersonalWebAPI.Entidades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControlPersonalWebAPI.Entidades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
